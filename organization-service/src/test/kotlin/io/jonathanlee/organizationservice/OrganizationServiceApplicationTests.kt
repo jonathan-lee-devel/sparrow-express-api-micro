f8c8f30305cb0c134b847b427272d8189543ca16
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-06-01 22:11:38 +0100

    Removed default test for organization-service

--- organization-service/src/test/kotlin/io/jonathanlee/organizationservice/OrganizationServiceApplicationTests.kt
+++ organization-service/src/test/kotlin/io/jonathanlee/organizationservice/OrganizationServiceApplicationTests.kt
@@ -1,13 +1,13 @@
 package io.jonathanlee.organizationservice
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class OrganizationServiceApplicationTests {
 
 	@Test
-	fun contextLoads() {
+	fun assertTrue() {
+		Assertions.assertTrue(true)
 	}
 
 }
